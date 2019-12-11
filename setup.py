3c67bad3fe3959a4c246a1bde115f1657d1d0d29
Mariya Sergeeva <46738637+islamariya@users.noreply.github.com>
2019-12-11 22:05:55 +0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Search_result_parser',
+    version='1.0',
+    packages=['HW01'],
+    url='',
+    license='GNU GENERAL PUBLIC LICENSE',
+    author='avantage',
+    author_email='',
+    description='It is console Python parser of search results of yandex.ru and google.com with user data input.'
+)
